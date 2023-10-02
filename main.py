998dd730edee41deb3825818f7cf305288c83124
Sergio1880 <88562736+Sergio1880@users.noreply.github.com>
2023-10-02 01:00:51 +0000

    Borrar todo

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
 
 
-print("Hola Mundo saludos desde Salta")
-
-
-x=int(input("ingrese numero:"))
-y=int(input("ingrese numero:"))
-z=x+y
-print(z)
-
-def suma(x,y):
-  z=x+y
-
-  return z
-
-
-
 
 #Hecho por Sergio Chavez 01/10
